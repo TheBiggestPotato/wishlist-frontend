6f4930d3ecb930b7afdf1a575ccf3a6142bb2cde
Andrei <andrei.fabian282@gmail.com>
2022-09-07 17:49:25 +0300

    constants folder

---
+++ src/components/Footer/Footer.constants.js
@@ -0,0 +1,22 @@
+export const links =[
+    {
+      Name: "GitHub",
+      href: "https://github.com/TheBiggestPotato"
+    }, 
+    {
+      Name: "React",
+      href: "https://reactjs.org/"
+    },
+    {
+      Name: "Node",
+      href: "https://nodejs.org/en/"
+    },
+    {
+      Name: "MySql",
+      href: "https://www.mysql.com/"
+    },
+    {
+      Name: "Softbinator",
+      href: "https://www.softbinator.com/"
+    },
+  ];

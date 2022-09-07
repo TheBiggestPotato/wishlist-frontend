2d5f6c4540b2a3edd9f33b5a8691033cac42ea10
Andrei <andrei.fabian282@gmail.com>
2022-09-07 18:03:21 +0300

    finished navbar

---
+++ src/components/NavBar/NavBar.constants.js
@@ -0,0 +1,29 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '/#',
+        className: 'navbar-link'
+    },
+    {
+        title: 'Lists',
+        url: '/#',
+        className: 'navbar-link'
+    },
+    {
+        title: 'Groups',
+        url: '/#',
+        className: 'navbar-link'
+    },
+    {
+        title: 'Profile',
+        url: '/#',
+        className: 'navbar-link'
+    },
+    {
+        title: 'Log in',
+        url: '/#',
+        className: 'navbar-link-mobile'
+    }
+];
+
+export const logo_src = `${process.env.PUBLIC_URL}/assets/logo.png`;
